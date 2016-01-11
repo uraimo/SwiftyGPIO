0661c78234d739d4f6a29f00f2f27e2c513bd3b9
Umberto Raimondi <me@uraimo.com>
2016-01-11 22:28:36 +0100

    Added a simple example
    This simple example reads the current values and perform a 0,1 cycle on
    GPIO0, put a led between GPIO0 and GND to see some action.

---
+++ Examples/Sources/main.swift
@@ -0,0 +1,22 @@
+import Glibc
+
+
+let id = 408
+var gp01 = GPIO(name: "P0",id: 408)
+print("Current Status")
+print("Direction: "+gp01.direction.rawValue)
+print("Edge: "+gp01.edge.rawValue)
+print("Active Low: "+String(gp01.activeLow))
+print("Value: "+String(gp01.value))
+gp01.direction = .OUT
+gp01.value = 1
+print("New Status")
+print("Direction: "+gp01.direction.rawValue)
+print("Edge: "+gp01.edge.rawValue)
+print("Active Low: "+String(gp01.activeLow))
+print("Value: "+String(gp01.value))
+
+repeat{
+	gp01.value = (gp01.value == 0) ? 1 : 0
+	usleep(150*1000)
+}while(true)
