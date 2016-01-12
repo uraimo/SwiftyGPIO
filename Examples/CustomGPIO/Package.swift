b79c8828490c6924c9f0c6c7808f8840c8bde7ab
Umberto Raimondi <me@uraimo.com>
2016-01-12 15:13:11 +0100

    Renamed example

--- Examples/CustomGPIO/Package.swift
+++ Examples/CustomGPIO/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GPIOTest",
+    name: "CustomGPIO",
     dependencies: [
         .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0),
     ]
