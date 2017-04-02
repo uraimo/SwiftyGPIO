ba85ea3bd14f03595e8e518b4a4332c5103f9af9
Umberto Raimondi <me@uraimo.com>
2017-04-02 18:26:07 +0200

    Added new R/W SPI implementation
    - Specifying the byte order is not possible anymore, the client of the
    library will need to perform the conversion
    - I'm now using the SPI_IOC_MESSAGE ioctl to perform write AND read, not
    simply writing to the sysfs spidev handle anymore
    - The odd clockDelay parameter has been replaced by a more useful
    frequency parameter
    - A few setter/getter for the SPI parameters have been added, I'm not
    sure how much they can be useful since all important parameters are
    aleady configured during the transfer call.
    - Reading is not implemented for the bit-banging SPI

--- Sources/SPI.swift
+++ Sources/SPI.swift
@@ -68,53 +68,197 @@ extension SwiftyGPIO {
 // MARK: SPI
 
 public protocol SPIOutput {
-    func sendData(_ values: [UInt8], order: ByteOrder, clockDelayUsec: Int)
+    // Send data at the requested frequency (from 500Khz to 20 Mhz)
+    func sendData(_ values: [UInt8], frequencyHz: UInt)
+    // Send data at the default frequency
     func sendData(_ values: [UInt8])
+    // Send data and then receive a chunck of data at the requested frequency (from 500Khz to 20 Mhz)
+    func sendDataAndRead(_ values: [UInt8], frequencyHz: UInt) -> [UInt8]
+    // Send data and then receive a chunck of data at the default frequency
+    func sendDataAndRead(_ values: [UInt8]) -> [UInt8]
+    // Returns true if the SPIOutput is using a real SPI pin, false if performing bit-banging
     func isHardware() -> Bool
 }
 
-public struct SysFSSPI: SPIOutput {
+/// Hardware SPI via SysFS
+public final class SysFSSPI: SPIOutput {
+
+    struct spi_ioc_transfer {
+        var tx_buf: UInt64
+        var rx_buf: UInt64
+        var len: UInt32
+        var speed_hz: UInt32 = 500000
+        var delay_usecs: UInt16 = 0
+        var bits_per_word: UInt8 = 8
+        let cs_change: UInt8 = 0
+        let tx_nbits: UInt8 = 0
+        let rx_nbits: UInt8 = 0
+        let pad: UInt16 = 0
+    }
+
     let spiId: String
+    var mode: CInt = 0
+    var bits: UInt8 = 8
+    var speed: UInt32 = 500000
+    var delay: UInt16 = 0
 
     public init(spiId: String) {
         self.spiId=spiId
         //TODO: Check if available?
     }
 
-    public func sendData(_ values: [UInt8], order: ByteOrder, clockDelayUsec: Int) {
-
-        if clockDelayUsec > 0 {
-            //Try setting new frequency in Hz
-            var frq: CInt = CInt(1_000_000 / Double(clockDelayUsec))
-            let fp = open(SPIBASEPATH+spiId, O_WRONLY | O_SYNC)
-            _ = ioctl(fp, SPI_IOC_WR_MAX_SPEED_HZ, &frq)
+    public func sendData(_ values: [UInt8], frequencyHz: UInt = 500000) {
+        if frequencyHz > 500000 {
+            speed = UInt32(frequencyHz)
         }
-
-        writeToFile(SPIBASEPATH+spiId, values:values)
+        transferData(SPIBASEPATH+spiId, tx:values)
     }
 
-    public func sendData(_ values: [UInt8]) {sendData(values, order:.MSBFIRST, clockDelayUsec:0)}
+    public func sendData(_ values: [UInt8]) {
+        sendData(values, frequencyHz: 500000)
+    }
+
+    public func sendDataAndRead(_ values: [UInt8], frequencyHz: UInt = 500000) -> [UInt8] {
+        if frequencyHz > 500000 {
+            speed = UInt32(frequencyHz)
+        }
+        let rx = transferData(SPIBASEPATH+spiId, tx:values)
+        return rx
+    }
+
+    public func sendDataAndRead(_ values: [UInt8]) -> [UInt8] {
+        return sendDataAndRead(values, frequencyHz: 500000)
+    }
 
     public func isHardware() -> Bool {
         return true
     }
 
-    private func writeToFile(_ path: String, values: [UInt8]) {
-        let fp = fopen(path, "w")
-        if fp != nil {
-            let ret = fwrite(values, MemoryLayout<CChar>.stride, values.count, fp)
-            if ret<values.count {
-                if ferror(fp) != 0 {
-                    perror("Error while writing to file")
-                    abort()
-                }
-            }
-            fclose(fp)
+    /// Write and read bits, will need a few dummy writes if you want only read
+    @discardableResult
+    private func transferData(_ path: String, tx: [UInt8]) -> [UInt8] {
+        let rx: [UInt8] = [UInt8](repeating:0, count: tx.count)
+
+        let fd = open(path, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
         }
+
+        var tr = spi_ioc_transfer(
+            tx_buf: UInt64( UInt(bitPattern: UnsafeMutablePointer(mutating: tx)) ),
+            rx_buf: UInt64( UInt(bitPattern: UnsafeMutablePointer(mutating: rx)) ),
+            len: UInt32(tx.count),
+            speed_hz: speed,
+            delay_usecs: delay,
+            bits_per_word: bits)
+
+        let r = ioctl(fd, SPI_IOC_MESSAGE1, &tr)
+        if r < 1 {
+            perror("Couldn't send spi message")
+            abort()
+        }
+        close(fd)
+
+        return rx
     }
+
+    public func setMode(_ to: CInt) {
+        mode = to
+
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_WR_MODE, &mode)
+        if r == -1 {
+            fatalError("Couldn't set spi mode")
+        }
+
+        close(fd)
+    }
+
+    public func getMode() -> CInt {
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+        if r == -1 {
+            fatalError("Couldn't get spi mode")
+        }
+
+        close(fd)
+        return mode
+    }
+
+    public func setBitsPerWord(_ to: UInt8) {
+        bits = to
+        
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits)
+        if r == -1 {
+            fatalError("Couldn't set bits per word")
+        }
+
+        close(fd)
+    }
+
+    public func getBitsPerWord() -> UInt8 {
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits)
+        if r == -1 {
+            fatalError("Couldn't get bits per word")
+        }
+
+        close(fd)
+        return bits
+    }
+
+    public func setMaxSpeedHz(_ to: UInt32) {
+        speed = to
+
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed)
+        if r == -1 {
+            fatalError("Couldn't set max speed hz")
+        }
+
+        close(fd)
+    }
+
+    public func getMaxSpeedHz() -> UInt32 {
+        let fd = open(SPIBASEPATH+spiId, O_RDWR)
+        guard fd > 0 else {
+            fatalError("Couldn't open the SPI device")
+        }
+
+        let r = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed)
+        if r == -1 {
+            fatalError("Couldn't get max speed hz")
+        }
+
+        close(fd)
+        return speed
+    }
+
 }
 
-public struct VirtualSPI: SPIOutput {
+/// Bit-banging virtual SPI implementation, output only
+public final class VirtualSPI: SPIOutput {
     let dataGPIO, clockGPIO: GPIO
 
     public init(dataGPIO: GPIO, clockGPIO: GPIO) {
@@ -126,38 +270,49 @@ public struct VirtualSPI: SPIOutput {
         self.clockGPIO.value = 0
     }
 
-    public func sendData(_ values: [UInt8], order: ByteOrder, clockDelayUsec: Int) {
+    public func sendData(_ values: [UInt8], frequencyHz: UInt = 500000) {
         let mmapped = dataGPIO.isMemoryMapped()
         if mmapped {
-            sendDataGPIOObj(values, order:order, clockDelayUsec:clockDelayUsec)
+            sendDataGPIOObj(values, frequencyHz: frequencyHz)
         } else {
-            sendDataSysFS(values, order:order, clockDelayUsec:clockDelayUsec)
+            sendDataSysFSGPIO(values, frequencyHz: frequencyHz)
         }
     }
+    public func sendData(_ values: [UInt8]) {
+        sendData(values, frequencyHz: 0)
+    }
 
-    private func sendDataGPIOObj(_ values: [UInt8], order: ByteOrder, clockDelayUsec: Int) {
+    public func sendDataAndRead(_ values: [UInt8], frequencyHz: UInt = 500000) -> [UInt8] {
+        self.sendData(values, frequencyHz: frequencyHz)
+        return [UInt8]()
+    }
+
+    public func sendDataAndRead(_ values: [UInt8]) -> [UInt8] {
+        return sendDataAndRead(values, frequencyHz: 0)
+    }
+
+    private func sendDataGPIOObj(_ values: [UInt8], frequencyHz: UInt) {
 
         var bit: Int = 0
         for value in values {
             for i in 0...7 {
-                switch order {
-                case .LSBFIRST:
-                    bit = ((value & UInt8(1 << i)) == 0) ? 0 : 1
-                case .MSBFIRST:
-                    bit = ((value & UInt8(1 << (7-i))) == 0) ? 0 : 1
-                }
+                bit = ((value & UInt8(1 << (7-i))) == 0) ? 0 : 1
 
                 dataGPIO.value = bit
                 clockGPIO.value = 1
-                if clockDelayUsec>0 {
-                    usleep(UInt32(clockDelayUsec))
+                if frequencyHz > 0 {
+                    let amount = UInt32(1_000_000/Double(frequencyHz))
+                    // Calling usleep introduces significant delay, don't sleep for small values
+                    if amount > 50 {
+                        usleep(amount)
+                    }
                 }
                 clockGPIO.value = 0
             }
         }
     }
 
-    private func sendDataSysFS(_ values: [UInt8], order: ByteOrder, clockDelayUsec: Int) {
+    private func sendDataSysFSGPIO(_ values: [UInt8], frequencyHz: UInt) {
 
         let mosipath = GPIOBASEPATH+"gpio"+String(self.dataGPIO.id)+"/value"
         let sclkpath = GPIOBASEPATH+"gpio"+String(self.clockGPIO.id)+"/value"
@@ -177,17 +332,16 @@ public struct VirtualSPI: SPIOutput {
         var bit: String = LOW
         for value in values {
             for i in 0...7 {
-                switch order {
-                case .LSBFIRST:
-                    bit = ((value & UInt8(1 << i)) == 0) ? LOW : HIGH
-                case .MSBFIRST:
-                    bit = ((value & UInt8(1 << (7-i))) == 0) ? LOW : HIGH
-                }
+                bit = ((value & UInt8(1 << (7-i))) == 0) ? LOW : HIGH
 
                 writeToFP(fpmosi, value:bit)
                 writeToFP(fpsclk, value:HIGH)
-                if clockDelayUsec>0 {
-                    usleep(UInt32(clockDelayUsec))
+                if frequencyHz > 0 {
+                    let amount = UInt32(1_000_000/Double(frequencyHz))
+                    // Calling usleep introduces significant delay, don't sleep for small values
+                    if amount > 50 {
+                        usleep(amount)
+                    }
                 }
                 writeToFP(fpsclk, value:LOW)
             }
@@ -206,18 +360,19 @@ public struct VirtualSPI: SPIOutput {
         }
     }
 
-    public func sendData(_ values: [UInt8]) {
-        self.sendData(values, order:.MSBFIRST, clockDelayUsec:0)
-    }
-
     public func isHardware() -> Bool {
         return false
     }
 }
 
 // MARK: - SPI Constants
-
+internal let SPI_IOC_WR_MODE: UInt = 0x40016b01
+internal let SPI_IOC_RD_MODE: UInt = 0x80016b01
+internal let SPI_IOC_WR_BITS_PER_WORD: UInt = 0x40016b03
+internal let SPI_IOC_RD_BITS_PER_WORD: UInt = 0x80016b03
 internal let SPI_IOC_WR_MAX_SPEED_HZ: UInt = 0x40046b04
+internal let SPI_IOC_RD_MAX_SPEED_HZ: UInt = 0x80046b04
+internal let SPI_IOC_MESSAGE1: UInt = 0x40206b00
 internal let SPIBASEPATH="/dev/spidev"
 
 // MARK: - Darwin / Xcode Support
