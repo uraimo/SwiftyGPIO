ae99789331c36cc6a66a7b6e39ed2eddd2b7bd07
Stephan Mantler <step@stephanmantler.com>
2017-09-27 19:05:39 +0000

    UART adaptations for modern Raspbian (#56)
    - Modernized tty naming ("serial0" / "serial1")
    - Added 2400 / 4800 speeds
    - refactored SysFSUART init() to take a list of devices to try in order of preference (first successful wins)

--- Sources/UART.swift
+++ Sources/UART.swift
@@ -33,7 +33,7 @@ extension SwiftyGPIO {
     public static func UARTs(for board: SupportedBoard) -> [UARTInterface]? {
         switch board {
         case .CHIP:
-            return [SysFSUART("S0")!]
+            return [SysFSUART(["serial0","ttyAMA0"])!]
         case .RaspberryPiRev1:
             fallthrough
         case .RaspberryPiRev2:
@@ -41,9 +41,10 @@ extension SwiftyGPIO {
         case .RaspberryPiPlusZero:
             fallthrough
         case .RaspberryPi2:
-            fallthrough
+            return [SysFSUART(["serial0","ttyAMA0"])!]
         case .RaspberryPi3:
-            return [SysFSUART("AMA0")!]
+            return [SysFSUART(["serial0","ttyS0"])!,
+                    SysFSUART(["serial1","ttyAMA0"])!]
         default:
             return nil
         }
@@ -111,6 +112,8 @@ public enum StopBits {
 }
 
 public enum UARTSpeed {
+    case S2400
+    case S4800
     case S9600
     case S19200
     case S38400
@@ -119,6 +122,12 @@ public enum UARTSpeed {
 
     public func configure(_ cfg: inout termios) {
         switch self {
+        case .S2400:
+            cfsetispeed(&cfg, speed_t(B2400))
+            cfsetospeed(&cfg, speed_t(B2400))
+        case .S4800:
+            cfsetispeed(&cfg, speed_t(B4800))
+            cfsetospeed(&cfg, speed_t(B4800))
         case .S9600:
             cfsetispeed(&cfg, speed_t(B9600))
             cfsetospeed(&cfg, speed_t(B9600))
@@ -144,22 +153,39 @@ public final class SysFSUART: UARTInterface {
     var tty: termios
     var fd: Int32
 
-    public init?(_ uartId: String) {
-        device = "/dev/tty"+uartId
-        tty = termios()
+    public init?(_ uartIdList: [String]) {
+        // try all items in list until one works
+        for uartId in uartIdList {
 
-        fd = open(device, O_RDWR | O_NOCTTY | O_SYNC)
-        guard fd>0 else {
-            perror("Couldn't open UART device")
-            abort()
-        }
+            device = "/dev/"+uartId
+            tty = termios()
 
-        let ret = tcgetattr(fd, &tty)
+            fd = open(device, O_RDWR | O_NOCTTY | O_SYNC)
+            guard fd>0 else {
+                if errno == ENOENT {
+                    // silently return nil if no such device
+                    continue
+                }
+                perror("Couldn't open UART device")
+                continue
+            }
 
-        guard ret == 0 else {
-            perror("Couldn't get terminal attributes")
-            abort()
-        }
+            let ret = tcgetattr(fd, &tty)
+
+            guard ret == 0 else {
+	        close(fd)
+                perror("Couldn't get terminal attributes")
+                continue
+            }
+
+            return
+	}
+
+        return nil
+    }
+
+    public convenience init?(_ uartId: String) {
+        self.init([uartId])
     }
 
     public func configureInterface(speed: UARTSpeed, bitsPerChar: CharSize, stopBits: StopBits, parity: ParityType) {
