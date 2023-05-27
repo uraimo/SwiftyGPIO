620f50998da33adf722eb9347e9c6891f15c2f5d
Jaswanth Ch <chennu.siva@gmail.com>
2023-05-27 12:45:19 +0530

    Update main.swift

--- Examples/Basic/raspi1PlusAndZero/Sources/BasicGPIO/main.swift
+++ Examples/Basic/raspi1PlusAndZero/Sources/BasicGPIO/main.swift
@@ -8,7 +8,9 @@ let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPiPlusZero)
 gpios[.P3]!.direction = .OUT
 while true {    
     gpios[.P3]!.value = 1
+    sleep(1)             //Requires Glibc library
     gpios[.P3]!.value = 0
+    sleep(1)
 }
 
 
